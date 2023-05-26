030a3810f3325e7af0a3fb7a5177f1e3e90ec8dc
mateusz.banasik <mateusz1691@gmail.com>
2023-05-26 11:59:34 +0200

    Update search spec

--- cypress/e2e/search.spec.cy.js
+++ cypress/e2e/search.spec.cy.js
@@ -1,54 +1,52 @@
-describe('Search Functionality', () => {
+describe('Search flow', () => {
+  before(() => {
+    cy.visit('');
+  });
 
-    before(() => {
-        cy.visit('');
-    })
+  it('Should add to wishlist', () => {
+    // on home page search for dragon
+    // click on heart on any product
+    // check if product is added to wishlist
 
-    it('Add to Wishlist', () => {
-        // on home page search for dragon
-        // click on heart on any product
-        // check if product is added to wishlist
+    cy.randomlyFailTest(Cypress.env('defualtFailureRate'));
+  });
 
-        cy.randomlyFailTest(Cypress.env("defualtFailureRate"));
-    })
+  it('Should filter search by color', () => {
+    // search for Cats
+    // select orange color
+    // displayed work count should be smaller
+    // orange color should be selected
 
-    it('Filter search by color', () => {
-      // search for Cats
-      // select orange color
-      // displayed work count should be smaller
-      // orange color should be selected
+    cy.randomlyFailTest(Cypress.env('defualtFailureRate'));
+  });
 
-        cy.randomlyFailTest(Cypress.env("defualtFailureRate"));
-    })
+  it('Should filter search by orientation', () => {
+    // search for Cats
+    // select vertical orientation
+    // displayed work count should be smaller
+    // vertical orientation should be selected
 
-    it('Filter search by orientation', () => {
-        // search for Cats
-        // select vertical orientation
-        // displayed work count should be smaller
-        // vertical orientation should be selected
+    cy.randomlyFailTest(Cypress.env('defualtFailureRate'));
+  });
 
-        cy.randomlyFailTest(Cypress.env("defualtFailureRate"));
-    })
+  it('Should filter search by type', () => {
+    // search for Cats
+    // select illustration type
+    // displayed work count should be smaller
+    // illustration type should be selected
 
-    it("Filter search by Type", () => {
-        // search for Cats
-        // select illustration type
-        // displayed work count should be smaller
-        // illustration type should be selected
+    cy.randomlyFailTest(Cypress.env('defualtFailureRate'));
+  });
 
-        cy.randomlyFailTest(Cypress.env("defualtFailureRate"));
-    });
+  it('Should use search buttom to find products', () => {
+    //search for cats
+    //scroll down to bottom
+    //page counter should be displayed
+    //related searches should be displayed
 
-    it("search bottom", () => {
-      //search for cats
-      //scroll down to bottom
-      //page counter should be displayed
-      //related searches should be displayed
+    cy.randomlyFailTest(Cypress.env('defualtFailureRate'));
+  });
 
-        cy.randomlyFailTest(Cypress.env("defualtFailureRate"));
-    })
-
-    //not tested
-    // forbbiden phrases - for example pokemon should retrun no results and display message to check offical brandshops and this might intres you section
-
-})
+  //not tested
+  // forbbiden phrases - for example pokemon should retrun no results and display message to check offical brandshops and this might intres you section
+});
